873c6d23027df846c03b6a210dca11dae0435536
Amrit Purshotam <amrit.purshotam@gmail.com>
2025-10-19 14:03:24 +0200

    Add types

--- tiger/data/sentence_embedding.py
+++ tiger/data/sentence_embedding.py
@@ -1,12 +1,13 @@
+from torch import Tensor
 from torch.utils.data import Dataset
 
 
 class SentenceEmbeddingsDataset(Dataset):
-    def __init__(self, data):
+    def __init__(self, data: Tensor):
         self.data = data
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.data.shape[0]
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> Tensor:
         return self.data[idx]
